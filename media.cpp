f4fb577732a2d847bd9c7ae0b9fe3231e1013cf5
sethueapen@gmail.com <sethueapen@gmail.com>
2018-10-22 22:01:14 -0700

    Second commit

---
+++ media.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+#include <media.h>
+
+using namespace std;
+
+Media::Media(char newTitle[]*, int newYear*){
+  Title = newTitle;
+  year = newYear;
+}
+int* Room::getYear(){
+  return year;
+}
+char[]* Room::getTitle(){
+  return Title;
+}
---
+++ media.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Media {
+ public:
+  Media(char[]*, int*);
+  int* getYear();
+  char[]* getTitle();
+ private:
+  int* year;
+  char Title[]*;
+};
